09a0fd46ae6789e8bb03493aeeaefd4d363b1901
Ada <adziklipa@gmail.com>
2018-06-28 14:06:08 +0100

    I added an on and off toggle button and rendering priorities in the form. I also made a submit button functional.

--- Section Code (Start)/src/App.vue
+++ Section Code (Start)/src/App.vue
@@ -97,19 +97,19 @@
             <hr>
             <div class="row">
                 <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                    <app-switch></app-switch>
+                    <app-switch v-model="dataSwitch"></app-switch>
                 </div>
             </div>
             <div class="row">
                 <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                     <button
-                            class="btn btn-primary">Submit!
+                            class="btn btn-primary" @click.prevent="submitted">Submit!
                     </button>
                 </div>
             </div>
         </form>
         <hr>
-        <div class="row">
+        <div class="row" v-if="isSubmitted">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <div class="panel panel-default">
                     <div class="panel-heading">
@@ -125,8 +125,8 @@
                             <li  v-for="item in sendMail">{{ item }}</li>
                         </ul>
                         <p>Gender: {{ gender }}</p>
-                        <p>Priority:</p>
-                        <p>Switched:</p>
+                        <p>Priority: {{ selectedPriority }}</p>
+                        <p>Switched: {{ dataSwitch }}</p>
                     </div>
                 </div>
             </div>
@@ -149,8 +149,15 @@
                 sendMail: [],
                 gender: 'Female',
                 selectedPriority: 'High',
-                priorities: ['High', 'Medium', 'Low']
+                priorities: ['High', 'Medium', 'Low'],
+                dataSwitch: true,
+                isSubmitted: false
             }
+        },
+        methods: {
+          submitted() {
+              this.isSubmitted = true;
+          }
         },
         components: {
             appSwitch: Switch
